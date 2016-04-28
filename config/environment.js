2defeba8372aa5c011ce900f33068433a06abecf
Michael Newman <daudadagr@gmail.com>
2016-04-27 23:37:07 -0500

    use hash in URL to allow refresh

--- config/environment.js
+++ config/environment.js
@@ -5,7 +5,7 @@ module.exports = function(environment) {
     modulePrefix: 'ember-tabbed-routes',
     environment: environment,
     baseURL: '/ember-tabbed-routes',
-    locationType: 'auto',
+    locationType: 'hash',
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
